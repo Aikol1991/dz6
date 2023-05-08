d8942dd15112c857932cb75828310ab8c003a20d
Aikol1991 <122163501+Aikol1991@users.noreply.github.com>
2023-05-09 01:38:05 +0600

    Add files via upload

---
+++ src/data/productsData.js
@@ -0,0 +1,52 @@
+const productsData = [
+    {
+        id: 1,
+        img: "./image/image1.jpg",
+        rating: "★★★★★",
+        title: "pineapple",
+        price: 69,
+        quantity: 1,
+    },
+    {
+        id: 2,
+        img: "./image/image2.jpg",
+        rating: "★★★★",
+        title: "strawberry",
+        price: 49,
+        quantity: 1,
+    },
+    {
+        id: 3,
+        img: "./image/image3.jpg",
+        rating: "★★★★★",
+        title: "cherry",
+        price: 29,
+        quantity: 1,
+    },
+    {
+        id: 4,
+        img: "./image/image4.jpg",
+        rating: "★★★★★",
+        title: "guava",
+        price: 79,
+        quantity: 1,
+    },
+    {
+        id: 5,
+        img: "./image/image5.jpg",
+        rating: "★★★★",
+        title: "avocado",
+        price: 59,
+        quantity: 1,
+    },
+    {
+        id: 6,
+        img: "./image/image6.jpg",
+        rating: "★★★★",
+        title: "pear",
+        price: 19,
+        quantity: 1,
+    },
+];
+
+export default productsData;
